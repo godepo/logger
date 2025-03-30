f82ceb9160ad0e93a86cac4cb5d5eddce5372658
bstern <krokodilgandy@gmail.com>
2025-03-30 23:52:59 +0300

    add wrap for logger api and base docs

--- logger_test.go
+++ logger_test.go
@@ -20,16 +20,22 @@ type Deps struct {
 }
 
 func newTestCase(t *testing.T) *groat.Case[Deps, State, context.Context] {
+	t.Helper()
 	tc := groat.New[Deps, State, context.Context](t,
 		func(t *testing.T, deps Deps) context.Context {
-			return context.WithValue(context.Background(), logInstance, deps.MockLogger)
+			t.Helper()
+
+			return Wrap(t.Context(), deps.MockLogger)
 		},
 		func(t *testing.T, deps Deps) Deps {
+			t.Helper()
 			deps.MockLogger = NewMockLogger(t)
+
 			return deps
 		},
 	)
 	tc.Go()
+
 	return tc
 }
 
@@ -84,37 +90,39 @@ func TestWarn(t *testing.T) {
 func TestWith(t *testing.T) {
 	t.Run("should be able to be able", func(t *testing.T) {
 		t.Run("when call empty context for debug", func(t *testing.T) {
-			ctx := With(context.Background(), slog.String("key", "value"))
+			ctx := With(t.Context(), slog.String("key", "value"))
 			Debug(ctx, "test")
 		})
 		t.Run("when call empty context for error", func(t *testing.T) {
-			ctx := With(context.Background(), slog.String("key", "value"))
+			ctx := With(t.Context(), slog.String("key", "value"))
 			Error(ctx, "test")
 		})
 		t.Run("when call empty context for warn", func(t *testing.T) {
-			ctx := With(context.Background(), slog.String("key", "value"))
+			ctx := With(t.Context(), slog.String("key", "value"))
 			Warn(ctx, "test")
 		})
 		t.Run("when call empty context for info", func(t *testing.T) {
-			ctx := With(context.Background(), slog.String("key", "value"))
+			ctx := With(t.Context(), slog.String("key", "value"))
 			Info(ctx, "test")
 		})
 	})
-
 }
 
 func ActLogDebug(_ *testing.T, deps Deps, state State) State {
 	deps.MockLogger.EXPECT().Debug(mock.Anything, state.Message, sliceToAny(state.Attrs)...)
+
 	return state
 }
 
 func ActLogWarn(_ *testing.T, deps Deps, state State) State {
 	deps.MockLogger.EXPECT().Warn(mock.Anything, state.Message, sliceToAny(state.Attrs)...)
+
 	return state
 }
 
 func ActLogError(_ *testing.T, deps Deps, state State) State {
 	deps.MockLogger.EXPECT().Error(mock.Anything, state.Message, sliceToAny(state.Attrs)...)
+
 	return state
 }
 
@@ -123,20 +131,24 @@ func sliceToAny[T any](a []T) []any {
 	for i, v := range a {
 		res[i] = v
 	}
+
 	return res
 }
 
 func ActLogInfo(_ *testing.T, deps Deps, state State) State {
 	deps.MockLogger.EXPECT().Info(mock.Anything, state.Message, sliceToAny(state.Attrs)...)
+
 	return state
 }
 
 func ArrangeStringAttr(_ *testing.T, state State) State {
 	state.Attrs = append(state.Attrs, slog.String(uuid.NewString(), uuid.NewString()))
+
 	return state
 }
 
 func ArrangeMessageString(_ *testing.T, state State) State {
 	state.Message = uuid.NewString()
+
 	return state
 }
